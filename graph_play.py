2526f720928728837657abdeec1dcd813e058345
Howon Lee <howon@howonlee.com>
2015-01-23 15:48:06 -0800

    bigger state spaces...

--- graph_play.py
+++ graph_play.py
@@ -3,8 +3,11 @@
 from graph_tool.all import *
 
 if __name__ == "__main__":
+    #what is the smallest step that you could take?
     g = Graph()
     v1 = g.add_vertex()
     g2 = Graph(g)
     v2 = g.add_vertex()
-    graph_draw(g2)
+    e = g.add_edge(v1, v2)
+    print(v1.out_degree())
+    #graph_draw(g, vertex_text=g.vertex_index)
