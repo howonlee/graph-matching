5f397e09ab302c424958ab32f4f8485f5b6a0edd
Howon Lee <howon@howonlee.com>
2015-01-24 12:27:31 -0800

    that is really fucking fast

--- edgelist.jl
+++ edgelist.jl
@@ -1,25 +1,39 @@
 using Graphs
 
-function read_edgelist(fname):
-  #instantiate the graph
+function read_edgelist(fname)
   max_vnum = -1
-  #open the file first time
-  #count the max_vnum
-  
-  #open the file second itme
-  #do the actual reading
-  graph
+  function cmp_vnum(str)
+    num = parseint(str)
+    num > max_vnum ? max_vnum = num : nothing
+    nothing
+  end
+  edges = Edge{Int}[]
+  i = 0
+  open(fname) do file
+    for line in eachline(file)
+      i += 1
+      first, second = split(line)
+      cmp_vnum(first)
+      cmp_vnum(second)
+      edges = push!(edges, Edge{Int}(i, parseint(first), parseint(second)))
+    end
+  end
+  g = simple_edgelist(max_vnum,edges)
 end
 
-function list_eo_cands(graph):
+function list_eo_cands(graph)
   cands = []
   cands
 end
 
-function eo():
+function eo()
+  nothing
   #funky version of the extremal optimization algorithm
 end
 
-function propagation_step(graph, eo_matching):
+function propagation_step(graph, eo_matching)
   prop = [] #do the propagation step
 end
+
+g = read_edgelist("turb.edgelist")
+show(g)
