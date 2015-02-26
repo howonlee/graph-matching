0ddea7489e94e16d18a84060aca6e1f7051badb3
Howon Lee <howon@howonlee.com>
2015-02-25 23:46:18 -0800

    desultory work on show_graph

--- show_graph.py
+++ show_graph.py
@@ -4,9 +4,3 @@ import matplotlib.pyplot as plt
 if __name__ == "__main__":
     net1 = nx.read_edgelist("./gen_graph1")
     net2 = nx.read_edgelist("./gen_graph2")
-    plt.figure()
-    plt.subplot(211)
-    nx.draw(net1)
-    plt.subplot(212)
-    nx.draw(net2)
-    plt.show()
