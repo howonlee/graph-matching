53f73455bf2967d1e887b2e41b7b1ca38fa86ceb
Howon Lee <howon@howonlee.com>
2015-02-26 23:31:31 -0800

    get rid of that redundant thought

--- gen_splitwords.py
+++
@@ -1,9 +0,0 @@
-from nltk.corpus import brown
-first = brown.words()[580596:]
-second = brown.words()[:580596]
-
-def bigrams(ls):
-    return zip(ls, ls[1:])
-
-first_bi = bigrams(first)
-print first_bi[:1000]
