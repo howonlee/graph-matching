640858f5d202eeef5227680e2e78ab1f2dde49cf
Howon Lee <howon@howonlee.com>
2015-02-28 09:06:53 -0800

    to inspection there is a similarity

--- display_matching.py
+++ display_matching.py
@@ -1,12 +1,12 @@
-import igraph as ig
+import networkx as nx
+import numpy as np
+import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
-    g = ig.Graph()
-    g.add_vertices(65)
-    with open("./gen_graph1", "r") as g1_file:
-        for line in g1_file:
-            first, second = map(int, line.split())
-            g.add_edges([(first, second)])
-    print "start..."
-    ig.plot(g, "g1_plot.pdf", layout=g.layout_lgl())
-    print "end..."
+    net1 = nx.read_edgelist("./gen_graph1")
+    net2 = nx.read_edgelist("./gen_graph2")
+    plt.matshow(nx.to_numpy_matrix(net1))
+    plt.title("1")
+    plt.matshow(nx.to_numpy_matrix(net2))
+    plt.title("2")
+    plt.show()
