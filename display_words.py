6e166d43e79fa69db574d740e0c6590931b90b50
Howon Lee <howon@howonlee.com>
2015-02-28 21:39:36 -0800

    display words exists, now match them up

--- display_words.py
+++ display_words.py
@@ -1,4 +1,12 @@
 import json
+import pickle
 
 if __name__ == "__main__":
-    pass
+    first_dict, second_dict, match_dict = {}, {}, {}
+    with open("first_dict.pickle", "r") as first_file:
+        first_dict = pickle.load(first_file)
+    with open("second_dict.pickle", "r") as second_file:
+        second_dict = pickle.load(second_file)
+    with open("prop_result.json", "r") as match_file:
+        match_dict = json.load(match_file)
+    print match_dict
